46fcb39592e5fd435ee9056d2b131695342f4870
GlaysonLuiz1 <Glaysonluiz1@gmail.com>
2022-02-21 11:42:24 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,44 @@
+// Button.stories.js|jsx
+
+import React from 'react';
+
+import { Button } from './Button';
+
+export default {
+  /* 👇 The title prop is optional.
+  * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
+  * to learn how to generate automatic titles
+  */
+  title: 'Button',
+  component: Button,
+
+};
+
 function HomePage() {
   return (
     <div>
       <h1>MM Recordes</h1>
+      <h2>Gravações e mixagens</h2>
+
+      <style jsx>{`
+      * {
+        color: blue;
+      }
+    `}</style>
+
+      < Button
+        label="Button"
+        onClick={() => { }
+        }
+        size="small"
+      />
 
     </div>)
+
+
+
+
+
 }
 
 export default HomePage
